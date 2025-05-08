5ca1ebdac2ade8ffc9881a1e07b2e80705abaea3
Jaksa Vucicevic <jaksa.vucicevic@gmail.com>
2025-05-08 16:32:56 +0200

    cleaning up some more

---
+++ comp1.hubbard.seed/tb_params.py
@@ -0,0 +1,21 @@
+#Emery model parameters
+
+Edc = 0
+epsd = 1.582 - Edc
+# -1.538
+epsp = 0
+tpd = 1.094
+tpp = 0.616
+tppp = 0.152
+mu = 0
+
+
+print(
+  "mu:",mu,
+  "epsd:",epsd,
+  "epsp:",epsp,
+  "tpd:",tpd,
+  "tpp:",tpp,
+  "tppp:",tppp
+)
+
---
+++ comp11a.hubbard.seed/tb_params.py
@@ -0,0 +1,20 @@
+#Emery model parameters
+
+Edc = 0# this is for Hubbard. for Emery use 3.12 #double counting shift
+epsd = 1.87 - Edc
+epsp = 0
+tpd = 1.29
+tpp = 0.674
+tppp = 0.184
+mu = 8
+
+
+print(
+  "mu:",mu,
+  "epsd:",epsd,
+  "epsp:",epsp,
+  "tpd:",tpd,
+  "tpp:",tpp,
+  "tppp:",tppp
+)
+
